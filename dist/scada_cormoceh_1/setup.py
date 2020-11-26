70728f3454e4ccbf59a31658fc393f4987253e54
Bulat Badmaev <b.badmaev@BINLTD.local>
2020-11-26 10:45:18 +0800

    del unneccery files

--- dist/scada_cormoceh_1/setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-# import xml.etree.ElementTree
-import py2exe
-setup(windows=[{"script":"scada_cormoceh_1.py"}], options={"py2exe":{"includes":["PyQt4"],"includes":["sip"]}})
